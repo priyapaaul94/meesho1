d5d5ac5ab81ed95c16756f9c45459593317160a3
priyapaaul94 <114506002+priyapaaul94@users.noreply.github.com>
2023-07-16 20:51:26 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+import Western from "./components/Western"
+import WesternTop from './components/WesternTop';
+{
+  /* The following line can be included in your src/index.js or App.js file */
+}
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Jewellery from './components/Jewellery';
+import JewellAccessories from './components/JewellAccessories';
+
+function App() {
+  return (
+    <>
+    <Western/>
+    <WesternTop/>
+    <Jewellery/>
+    <JewellAccessories/>
+    
+
+    </>
+  );
+    
+}
+
+export default App;
